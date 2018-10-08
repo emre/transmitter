0bfec6c3f816981c435b4b34e607c0cccc27b148
Emre Yilmaz <mail@emreyilmaz.me>
2018-10-08 21:16:24 +0300

    Package the app

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='witness_transmitter',
+    version='0.0.1',
+    packages=["transmitter",],
+    url='http://github.com/emre/transmitter',
+    license='MIT',
+    author='emre yilmaz',
+    author_email='mail@emreyilmaz.me',
+    description='STEEM Witness updates made easy.',
+    entry_points={
+        'console_scripts': [
+            'transmitter = transmitter.main:main',
+        ],
+    },
+    install_requires=["beem", "logme"]
+)
