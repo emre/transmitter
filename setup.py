802bc17876b0dad0784b3e79bacf311bc1f47511
Emre Yilmaz <mail@emreyilmaz.me>
2018-11-20 23:57:51 +0300

    make transmitter also a 'package'

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='transmitter',
-    version='0.1.2',
+    version='0.1.3',
     packages=[
         'transmitter',
         'transmitter.pricefeed',
