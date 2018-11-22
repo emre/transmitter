5c77948b64511a6e47aa782cd946863549ad661d
Emre Yilmaz <mail@emreyilmaz.me>
2018-11-22 20:47:47 +0300

    Add outlier detection
    - Only works if at least 3 markets are selected
    - Uses a slightly different "modified Z-Score" algorithm

--- transmitter/pricefeed/markets.py
+++ transmitter/pricefeed/markets.py
@@ -1,7 +1,9 @@
+import numpy as np
+
 from .adapters.binance import BinanceAdapter
-from .adapters.poloniex import PoloniexAdapter
 from .adapters.bittrex import BittrexAdapter
 from .adapters.huobi import HuobiAdapter
+from .adapters.poloniex import PoloniexAdapter
 from .adapters.upbit import UpbitAdapter
 
 AVAILABLE_MARKETS = {
@@ -12,6 +14,8 @@ AVAILABLE_MARKETS = {
     "upbit": UpbitAdapter,
 }
 
+DEFAULT_MARKETS = list(AVAILABLE_MARKETS.keys())
+
 
 def is_marketlist_valid(market_list):
     # that's a crazy one liner.
@@ -20,9 +24,28 @@ def is_marketlist_valid(market_list):
     return len(set(market_list).difference(set(AVAILABLE_MARKETS.keys()))) == 0
 
 
-def get_average_price(markets):
-    prices = [
+def get_prices(markets):
+    return [
         AVAILABLE_MARKETS.get(market)().get_price() for market in markets]
+
+
+def get_average_price(prices):
     return round(sum(prices) / len(prices), 3)
 
 
+def exclude_outliers(prices, threshold=35):
+    """
+    An outlier logic to exclude bad markets.
+    Uses a slightly modified version of "Modified Z-score".
+    https://www.itl.nist.gov/div898/handbook/eda/section3/eda35h.htm
+    """
+    if isinstance(prices, list):
+        prices = np.array(prices)
+    original_median = np.median(prices)
+    deviation = np.abs(prices - original_median)
+    median_deviation = np.median(deviation)
+    score = 0
+    if median_deviation:
+        score = deviation / median_deviation
+    filtered_prices = prices[score < threshold]
+    return list(filtered_prices)
